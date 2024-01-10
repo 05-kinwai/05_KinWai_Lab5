f21712d0b35d9fb3353a28dd9f5e0aa66dec921f
25-Jay <ang.ying.jie.jay@Gmail.com>
2024-01-10 22:44:01 +0800

    changes

---
+++ 05_KinWai_Lab5/Assets/Scripts/Coin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Coin : MonoBehaviour
+{
+
+    public GameObject coin;
+    public int rotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotateSpeed = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(coin);
+        }
+    }
+}
